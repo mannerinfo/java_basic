8a57090fcf80025fcac3fab645862e42a672134c
KD <123051736+mannerinfo@users.noreply.github.com>
2023-05-31 15:03:23 +0530

    Adding "Hello, World" File - Java
    File contain Hello, World output in java language...

---
+++ hello_world.java
@@ -0,0 +1,37 @@
+// Class "hello, World" is the name of our file, hence it is must to keep the name of main class as of file.....
+// Every instructions to run the code is done under class in Java.....
+// The below is the main class of our "Hello, World" program.....
+
+class hello_world{
+
+// In the following statement the meaning of each is as the following.....
+
+public static void main(String[] args) {
+ System.out.println("Hellow, World");   
+}
+}
+
+// public: It is an access modifier that specifies that the main method can be accessed from anywhere, allowing it to be called by the JVM.
+// static: It is a keyword that indicates that the main method belongs to the class itself and can be called without creating an instance of the class.
+// void: It is the return type of the main method, indicating that the method does not return any value.
+// main: It is the name of the method. The JVM looks for this method to start the execution of the program.
+// String[] args: It is a parameter of the main method. It represents an array of strings that can be passed as command-line arguments when executing the Java program. The arguments can be used within the program as needed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
